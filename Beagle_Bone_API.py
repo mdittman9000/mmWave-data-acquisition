fba61843d6f9a48e0480e4824fcbe4af44a9b84b
cadepoland <63059376+cadepoland@users.noreply.github.com>
2021-11-03 19:17:49 -0400

    Create Beagle_Bone_API.py

---
+++ Beagle_Bone_API.py
@@ -0,0 +1,158 @@
+####################################################################################################
+#Code from Sudharshan and TI for connecting COM Ports and sending Configuration file (need to test)
+####################################################################################################
+import struct
+import sys
+import serial
+import binascii
+import time
+import numpy as np
+import math
+
+class uartParserSDK():
+  #find various utility functions here for connecting to COM Ports, send data, etc...
+  #connect to com ports
+  # Call this function to connect to the comport. This takes arguments self (intrinsic), uartCom, and dataCom. No return, but sets internal variables in the parser object.
+  def connectComPorts(self, uartCom, dataCom):
+      self.uartCom = serial.Serial(uartCom, 115200,parity=serial.PARITY_NONE,stopbits=serial.STOPBITS_ONE,timeout=0.3)
+      if (self.capon3D == 1 and self.aop == 0):
+          self.dataCom = serial.Serial(dataCom, 921600*1,parity=serial.PARITY_NONE,stopbits=serial.STOPBITS_ONE,timeout=0.025)
+      else:
+          self.dataCom = serial.Serial(dataCom, 921600,parity=serial.PARITY_NONE,stopbits=serial.STOPBITS_ONE,timeout=0.025)
+      self.dataCom.reset_output_buffer()
+      print('Connected')
+
+  #send cfg over uart
+  def sendCfg(self, cfg):
+      for line in cfg:
+          time.sleep(.1)
+          self.uartCom.write(line.encode())
+          ack = self.uartCom.readline()
+          print(ack)
+          ack = self.uartCom.readline()
+          print(ack)
+      time.sleep(3)
+      self.uartCom.reset_input_buffer()
+      self.uartCom.close()
+    
+    
+####################################################################################################
+#Camera Code developed by us (Team 4 from MSU)
+####################################################################################################
+import numpy as np
+import os
+import time
+import cv2
+
+
+filename = 'video.avi'
+frames_per_second = 24.0
+res = '720p'
+
+# Set resolution for the video capture
+# Function adapted from https://kirr.co/0l6qmh
+def change_res(cap, width, height):
+    cap.set(3, width)
+    cap.set(4, height)
+
+# Standard Video Dimensions Sizes
+STD_DIMENSIONS =  {
+    "480p": (640, 480),
+    "720p": (1280, 720),
+    "1080p": (1920, 1080),
+    "4k": (3840, 2160),
+}
+
+
+# grab resolution dimensions and set video capture to it.
+def get_dims(cap, res='1080p'):
+    width, height = STD_DIMENSIONS["480p"]
+    if res in STD_DIMENSIONS:
+        width,height = STD_DIMENSIONS[res]
+    ## change the current caputre device
+    ## to the resulting resolution
+    change_res(cap, width, height)
+    return width, height
+
+# Video Encoding, might require additional installs
+# Types of Codes: http://www.fourcc.org/codecs.php
+VIDEO_TYPE = {
+    'avi': cv2.VideoWriter_fourcc(*'XVID'),
+    #'mp4': cv2.VideoWriter_fourcc(*'H264'),
+    'mp4': cv2.VideoWriter_fourcc(*'XVID'),
+}
+
+def get_video_type(filename):
+    filename, ext = os.path.splitext(filename)
+    if ext in VIDEO_TYPE:
+      return  VIDEO_TYPE[ext]
+    return VIDEO_TYPE['avi']
+
+
+def start_video_acquisition():
+    """
+    Function that starts video acquisition. This will serve as the entry point
+    to the camera data
+    :return: None
+    """
+    cap = cv2.VideoCapture(0)
+    out = cv2.VideoWriter(filename, get_video_type(filename), 25, get_dims(cap, res))
+
+    while True:
+        ret, frame = cap.read()
+        out.write(frame)
+        cv2.imshow('frame',frame)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+
+def start_video_acquisition_timed(seconds=10):
+    """
+    Function that starts video acquisition. This will serve as the entry point
+    to the camera data
+    :param seconds: The number of seconds to capture video for (Default value of 10 seconds)
+    :return: None
+    """
+
+    # Get the current time
+    start_time = time.time()
+
+    # Store the elapsed time
+    elapsed_time = time.time() - start_time
+
+    cap = cv2.VideoCapture(0)
+    out = cv2.VideoWriter(filename, get_video_type(filename), 25, get_dims(cap, res))
+
+    # While the amount of time passed is less than how long we want to record video for
+    while elapsed_time < seconds:
+
+        # Capture the frame
+        ret, frame = cap.read()
+
+        # Write the frame to the screen (not necessary for Released version)
+        out.write(frame)
+
+        # Font for time
+        font = cv2.FONT_HERSHEY_SIMPLEX
+
+        # What is the current time?
+        current_time = time.gmtime(time.time())
+
+        # Format the time
+        current_time_formatted = str(time.strftime("%Y-%m-%d %H:%M:%S", current_time))
+
+        cv2.putText(frame, current_time_formatted, (10, 30), font, 1, (255, 255, 255), 2, cv2.LINE_AA)
+
+        # Show the frame on the screen
+        cv2.imshow('frame', frame)
+
+        # Calculate the time elapsed
+        elapsed_time = int(time.time() - start_time)
+
+        # Allow for break key (testing on windows)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    cap.release()
+    out.release()
+    cv2.destroyAllWindows()
